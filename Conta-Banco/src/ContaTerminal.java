c5ed7dde02dff57bc089513e52be4088b3c05099
Lima <jvbarb8@gmail.com>
2024-06-24 22:46:10 -0300

    Primeiro-Projeto

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,12 @@
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        //TODO:Conhecer e importar a classe Scanner
+
+        //Exibir as mensagem para nosso usuario
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mensagem conta criada
     }
 }
